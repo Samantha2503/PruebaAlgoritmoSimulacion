392ff6518d3dbacb104db9ab94bd7239e7df1fc3
Samantha <Samantha@USUARIO-FGHUI5M>
2024-10-07 17:07:21 -0600

    ok

---
+++ PruebaAlgoritmoSimulacion/Clases/Arco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaAlgoritmoSimulacion.Clases
+{
+    public class Arco
+    {
+        //Nace Id Punto
+        public int IdPuntoOrigen { get; set; }
+        public int IdPuntoDestino { get; set; }
+        public double Distancia { get; set; }
+        public double Competitividad {  get; set; }
+
+        public Arco() 
+        {
+
+        }
+        public Arco(Arco arco)
+        {
+            IdPuntoDestino=arco.IdPuntoOrigen;
+            IdPuntoOrigen=arco.IdPuntoDestino;
+            Distancia=arco.Distancia;
+            Competitividad=arco.Competitividad;
+        }
+
+    }
+}
