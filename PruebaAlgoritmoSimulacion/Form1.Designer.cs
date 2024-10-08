5d678b276e34da89c4061f5124827b918e27dea7
Samantha <Samantha@USUARIO-FGHUI5M>
2024-10-07 18:45:31 -0600

    Cambios

--- PruebaAlgoritmoSimulacion/Form1.Designer.cs
+++ PruebaAlgoritmoSimulacion/Form1.Designer.cs
@@ -28,19 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(450, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Ejecutar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(286, 155);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(161, 161);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Máximo";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(161, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Puntos totales";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(286, 81);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(285, 261);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 5;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
     }
 }
--- PruebaAlgoritmoSimulacion/Form1.cs
+++ PruebaAlgoritmoSimulacion/Form1.cs
@@ -1,3 +1,7 @@
+using PruebaAlgoritmoSimulacion.Algoritmos;
+using PruebaAlgoritmoSimulacion.Clases;
+using System.Windows.Forms;
+
 namespace PruebaAlgoritmoSimulacion
 {
     public partial class Form1 : Form
@@ -11,5 +15,49 @@ namespace PruebaAlgoritmoSimulacion
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Paso 0: condicion vacia
+            if (textBox1.Text.Equals("") || textBox2.Text.Equals(""))
+            {
+                MessageBox.Show("Los numeros deben ser mayores a 0");
+            }
+
+            //Paso 1: Inicializa parámetros
+            int puntosTotales = Convert.ToInt32(textBox1.Text);
+            int maximo = Convert.ToInt32(textBox2.Text);
+            //Paso 2: llamar algoritmo
+            GeneradorAleatorios generador = new GeneradorAleatorios();
+            List<Asignacion> listaSalida = generador.CrearListaOrigen(puntosTotales, 0, maximo);
+            llenarGrid(listaSalida);
+        }
+
+        private void label1_Click(List<Asignacion> lista)
+        {
+
+        }
+
+        public void llenarGrid(List<Asignacion> lista)
+        {
+            //Paso 0: Indicas el número de columnas 
+            string numeroColumna1 = "1";
+            string numeroColumna2 = "2";
+            string numeroColumna3 = "3";
+            //Paso 1: Determinas la cantidad de columnas 
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add(numeroColumna1, "Id");
+            dataGridView1.Columns.Add(numeroColumna2, "Latitud");
+            dataGridView1.Columns.Add(numeroColumna3, "Longitud");
+            //Paso 2: Recorrer el grid para cada fila y llenar de valores aleatorios
+            for (int i = 0; i < lista.Count; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna1) - 1].Value = (lista[i].IdPunto).ToString();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna2) - 1].Value = lista[i].Latitud.ToString();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna3) - 1].Value = lista[i].Longitud.ToString();
+            }
+        }
     }
 }
+
